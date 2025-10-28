673d40d817a86d1963879aad5a1844e153f2734b
LanCute2212 <phongvan1908@gmail.com>
2025-10-28 14:07:47 +0700

    update

--- app/src/main/java/com/example/caloriesapp/apiclient/ApiClient.java
+++ app/src/main/java/com/example/caloriesapp/apiclient/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://localhost:8081/";
+    private static final String BASE_URL = "http://172.17.128.107:8081/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
